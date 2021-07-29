ef36ffff1885f5f303f53988469210cac1fea374
karim <k.guezlane@gmail.com>
2021-07-29 20:44:41 +0100

    imorted the general info component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {GeneralInfo} from './components/general_information'
 function App() {
   return (
     <div className="App">
+      <GeneralInfo/>
     </div>
   );
 }
