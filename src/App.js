b1c82b287de346c811153adb9e1339cd9aab73e3
karim <k.guezlane@gmail.com>
2021-07-31 20:33:12 +0100

    added Experience part to the CV

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './style/App.css';
-import {GeneralInfo} from './components/general_information'
-import {BodyComponent} from './components/body_component'
+import "./style/App.css";
+import { GeneralInfo } from "./components/general_information";
+import { BodyComponent } from "./components/body_component";
 function App() {
-  return (
-    <div className="App">
-      <div id="cvContainer">
-        <GeneralInfo/>
-        <BodyComponent name="Education"/>
-      </div>    
-    </div>
-  );
+	return (
+		<div className="App">
+			<div id="cvContainer">
+				<GeneralInfo />
+				<BodyComponent name="Experience" />
+				<BodyComponent name="Education" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
