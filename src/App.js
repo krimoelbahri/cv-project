e90aada0c63d694dc32b8161be5d895c703d6e39
karim <k.guezlane@gmail.com>
2021-08-01 18:01:12 +0100

    added delete info method and preview mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import "./style/App.css";
 import { GeneralInfo } from "./components/General_information";
 import { BodyComponent } from "./components/Body_component";
-function App() {
-	return (
-		<div className="App">
-			<div id="cvContainer">
-				<GeneralInfo />
-				<BodyComponent
-					name="Experience"
-					place="Company"
-					title="Position"
-				/>
-				<BodyComponent
-					name="Education"
-					place="Establishment"
-					title="Degree"
-				/>
+import { SkillsComponent } from "./components/Skills_component";
+import React from "react";
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.edit = this.edit.bind(this);
+		this.preview = this.preview.bind(this);
+
+		this.state = {
+			isPreview: false,
+		};
+	}
+	edit() {
+		this.setState({
+			isPreview: false,
+		});
+	}
+	preview() {
+		this.setState({
+			isPreview: true,
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className="buttons-div">
+					<button onClick={this.edit}>Edit Mode</button>
+					<button onClick={this.preview}>Preview Mode</button>
+				</div>
+				<div id="cvContainer">
+					<GeneralInfo isPreview={this.state.isPreview} />
+					<BodyComponent
+						name="Experience"
+						place="Company"
+						title="Position"
+						isPreview={this.state.isPreview}
+					/>
+					<BodyComponent
+						name="Education"
+						place="Establishment"
+						title="Degree"
+						isPreview={this.state.isPreview}
+					/>
+					<SkillsComponent
+						name="Skills"
+						isPreview={this.state.isPreview}
+					/>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/Body_component.js
+++ src/components/Body_component.js
@@ -8,6 +8,8 @@ class BodyComponent extends React.Component {
 		this.handleClick = this.handleClick.bind(this);
 		this.onSubmitted = this.onSubmitted.bind(this);
 		this.onCancel = this.onCancel.bind(this);
+		this.deleteInformation = this.deleteInformation.bind(this);
+
 		this.state = {
 			isEditing: false,
 			info: [],
@@ -29,9 +31,16 @@ class BodyComponent extends React.Component {
 			isEditing: !prevState.isEditing,
 		}));
 	}
+	deleteInformation(obj) {
+		this.setState(() => ({
+			info: this.state.info.filter((object) => object !== obj),
+		}));
+	}
 	render() {
 		let { isEditing, info } = this.state;
+		let isPreview = this.props.isPreview;
 		let form;
+		let body;
 		if (isEditing) {
 			form = (
 				<Form
@@ -49,17 +58,35 @@ class BodyComponent extends React.Component {
 				</button>
 			);
 		}
-		return (
-			<div className="body-info">
-				<h2>{this.props.name}</h2>
-				<Information
-					info={info}
-					place={this.props.place}
-					title={this.props.title}
-				/>
-				{form}
-			</div>
-		);
+		if (!isPreview) {
+			body = (
+				<div className="body-info-full">
+					<h2>{this.props.name}</h2>
+					<Information
+						info={info}
+						place={this.props.place}
+						title={this.props.title}
+						isPreview={isPreview}
+						deleteClick={this.deleteInformation}
+					/>
+					{form}
+				</div>
+			);
+		} else {
+			body = (
+				<div className="body-info-full">
+					<h2>{this.props.name}</h2>
+					<Information
+						info={info}
+						place={this.props.place}
+						title={this.props.title}
+						isPreview={isPreview}
+						deleteClick={this.deleteInformation}
+					/>
+				</div>
+			);
+		}
+		return <div className="body-info">{body}</div>;
 	}
 }
 export { BodyComponent };
