85358c8843d857b75d084b04f8ddb4764deb08ea
karim <k.guezlane@gmail.com>
2021-08-01 13:16:21 +0100

    Formated with Prettier

--- src/components/Input_component.js
+++ src/components/Input_component.js
@@ -1,16 +1,16 @@
-import React from 'react'
-class InputComponent extends React.Component{
-    render(){
-        return(
-            <div className="form-div pd-1px">
-                <label htmlFor={this.props.id}>{this.props.title}</label>
-                <input 
-                onChange={this.props.handleInputChange}
-                id={this.props.id}
-                type={this.props.type}
-                 />
-            </div>
-        )
-    }
+import React from "react";
+class InputComponent extends React.Component {
+	render() {
+		return (
+			<div className="form-div pd-1px">
+				<label htmlFor={this.props.id}>{this.props.title}</label>
+				<input
+					onChange={this.props.handleInputChange}
+					id={this.props.id}
+					type={this.props.type}
+				/>
+			</div>
+		);
+	}
 }
-export {InputComponent}
+export { InputComponent };
